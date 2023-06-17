486fcb9486fab5871521506b7a7a4a67cb10064f
LoganKaval <136924755+LoganKaval@users.noreply.github.com>
2023-06-17 13:10:24 -0500

    Add files via upload

---
+++ SM-AppSearcher.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo Hello, I am SM or system Monitor.
+sleep 5
+echo Type the directory for the application you want to run.
+sleep 5
+echo You will only have ten seconds after this message.
+sleep 5
+read=app
+sleep 10
+$app
